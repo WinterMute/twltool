0abaad5ac3e977f12fcd00014601a3437f82fbee
Dave Murphy <davem@devkitpro.org>
2016-12-13 19:27:04 +0000

    use stdbool.h and stdint.h types

--- types.h
+++ types.h
@@ -2,18 +2,18 @@
 #define __TYPES_H__
 
 #include <stdint.h>
+#include <stdbool.h>
 
-typedef unsigned char		u8;
-typedef unsigned short		u16;
-typedef unsigned int		u32;
-typedef unsigned long long	u64;
+typedef uint8_t   u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
 
-typedef signed char			s8;
-typedef signed short		s16;
-typedef signed int			s32;
-typedef signed long long	s64;
+typedef int8_t    s8;
+typedef int16_t  s16;
+typedef int32_t  s32;
+typedef int64_t  s64;
 
-typedef enum { false, true } bool;
 
 enum flags
 {
